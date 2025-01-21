5d93eb9f04d13145f2ebf504d14fb601856be884
Amal Amara <102389603+AmaraAmal@users.noreply.github.com>
2025-01-21 17:01:01 +0100

    Update index.js

--- Config/index.js
+++ Config/index.js
@@ -8,17 +8,17 @@ import { createClient } from '@supabase/supabase-js';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDdcjMfrGBSWFoW1YTkwxQ4UxqDLsojRys",
+  apiKey: "",
   authDomain: "whatsappclone-d4f84.firebaseapp.com",
   databaseURL: "https://whatsappclone-d4f84-default-rtdb.firebaseio.com",
   projectId: "whatsappclone-d4f84",
   storageBucket: "whatsappclone-d4f84.firebasestorage.app",
-  messagingSenderId: "248873935135",
-  appId: "1:248873935135:web:1301ebd55bbfd67cc8502a"
+  messagingSenderId: "",
+  appId: ""
 };
 
 const supabaseUrl = "https://duggzbjgkidkvrugumkx.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR1Z2d6Ympna2lka3ZydWd1bWt4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI1MzQyOTUsImV4cCI6MjA0ODExMDI5NX0.j4ekVNtYVBjOy3svX59dOXR0OWcI-zC8FIYEDQJKtCg";
+const supabaseKey = "";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 // Initialize Firebase
